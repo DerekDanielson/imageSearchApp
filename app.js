71779aba0f5f3fcc3c842c215afebd3a1ef95dc2
Derek <derekdanielson84@gmail.com>
2023-09-29 09:11:12 -0600

    Brought in elements

---
+++ app.js
@@ -0,0 +1,7 @@
+//API key
+const apiKey = 'yDnY5p4WGYIku_wXopzGe7ZRALyukrJrO3g19OexyVA';
+
+const formEl = document.querySelector('form');
+const searchInputEl = document.getElementById('search-input');
+const searchResultEl = document.querySelector('.search-results');
+const showMoreBtn = document.getElementById('show-more-btn');

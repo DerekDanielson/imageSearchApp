81534f8f451ec7624ff691ddaf31bcd9e3bbbbfe
Derek <derekdanielson84@gmail.com>
2023-10-02 09:20:50 -0600

    Added searchImages function

--- app.js
+++ app.js
@@ -6,8 +6,21 @@ const searchInputEl = document.getElementById('search-input');
 const searchResultEl = document.querySelector('.search-results');
 const showMoreBtn = document.getElementById('show-more-btn');
 
+let inputData = '';
+let page = 1;
+
+
+async function searchImages(){
+    inputData = searchInputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${apiKey}`;
+    
+    const response = await fetch(url);
+    const data = response.json();
+    console.log(data);
+};
 
 formEl.addEventListener('submit', (e)=>{
     e.preventDefault();
     searchImages();
 });
+
